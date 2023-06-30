61a35c2742f6c99836b0a055b2ed6a8b2cd2dbaf
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 00:48:00 -0700

    instantiated field and methods for repository class to access the datastore

--- repository/TradeAccountRepository.java
+++ repository/TradeAccountRepository.java
@@ -1,7 +1,27 @@
 package repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import pojo.TradeAccount;
 
 public class TradeAccountRepository {
 
+    private Map<String, TradeAccount> datastore = new HashMap<>();
 
+    public void createTradeAccount(TradeAccount account) {
+        this.datastore.put(account.getId(), account.clone());
+    }
+
+    public TradeAccount retrieveTradeAccount(String id) {
+        return this.datastore.get(id).clone();
+    }
+
+    public void updateTradeAccount(TradeAccount account) {
+        this.datastore.put(account.getId(), account.clone());
+    }
+
+    public void deleteTradeAccount(String id) {
+        this.datastore.remove(id);
+    }
 }
