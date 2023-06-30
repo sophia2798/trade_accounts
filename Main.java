ef162c78799f8c5ab8c0cfa8665072eb5a66678f
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 01:43:22 -0700

    executed methods in main method, checked output against provided answer output, correct

--- Main.java
+++ Main.java
@@ -24,7 +24,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        try {
+            loadTradeAccounts();
+            applyTransactions();
+            finalTest();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
 
     }
 
