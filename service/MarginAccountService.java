4796e41a97ae8a597eb4bb5763a00af95d5da1a2
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 01:09:16 -0700

    instantiated MarginAccountService class, override methods from interface

--- service/MarginAccountService.java
+++ service/MarginAccountService.java
@@ -1,7 +1,46 @@
 package service;
 
+import java.math.BigDecimal;
 
-public class MarginAccountService {
+import pojo.MarginAccount;
+import repository.TradeAccountRepository;
 
+public class MarginAccountService implements TradeAccountService{
+
+    private TradeAccountRepository repository;
+
+    public MarginAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public MarginAccount retrieveTradeAccount(String id) {
+        return (MarginAccount) this.repository.retrieveTradeAccount(id);
+    }
+
+    public void createTradeAccount(MarginAccount account) {
+        this.repository.createTradeAccount(account);
+    }
+
+    public void updateTradeAccount(MarginAccount account) {
+        this.repository.updateTradeAccount(account);
+    }
+
+    public void deleteTradeAccount(String id) {
+        this.repository.deleteTradeAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);
+        account.getMargin().add(amount);
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);
+        account.getMargin().subtract(amount);
+        updateTradeAccount(account);
+    }
 
 }
