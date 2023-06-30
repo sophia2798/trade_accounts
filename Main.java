8db9e382d90850b31944f9781392a9b8a6121743
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 01:32:32 -0700

    grabbing datastore from .txt files, created loadTradeAccounts method

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import constants.TradeAccountType;
+import constants.Transaction;
+import pojo.CashAccount;
+import pojo.MarginAccount;
+import pojo.TradeAccount;
+import repository.TradeAccountRepository;
+import service.CashAccountService;
+import service.MarginAccountService;
+
 public class Main {
+
+    static Path[] paths = new Path[] {Paths.get("data/accounts.txt"), Paths.get("data/transactions.txt")};
+
+
+    static TradeAccountRepository tradeAccountRepository = new TradeAccountRepository();
+    static CashAccountService cashAccountService = new CashAccountService(tradeAccountRepository);
+    static MarginAccountService marginAccountService = new MarginAccountService(tradeAccountRepository);
+
     public static void main(String[] args) {
-        
+
+
+
+    }
+
+    public void loadTradeAccounts() throws IOException {
+        Files.lines(paths[0])
+            .forEach(line -> {
+                String[] words = line.split(" ");
+                Boolean isCashAccount = words[0].equals(TradeAccountType.CASH.toString());
+                if (isCashAccount) cashAccountService.createTradeAccount(new CashAccount(words[1], new BigDecimal(words[2])));
+                else marginAccountService.createTradeAccount(new MarginAccount(words[1], new BigDecimal(words[2])));
+            });
     }
 }
