820ec776f4121916653d789ed2118a84648e86f2
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 01:05:47 -0700

    instantiated CashAccountService class, override methods from interface

--- service/CashAccountService.java
+++ service/CashAccountService.java
@@ -1,7 +1,46 @@
 package service;
 
+import java.math.BigDecimal;
 
-public class CashAccountService {
+import pojo.CashAccount;
+import repository.TradeAccountRepository;
 
+public class CashAccountService implements TradeAccountService{
+
+    private TradeAccountRepository repository;
+
+    public CashAccountService(TradeAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public CashAccount retrieveTradeAccount(String id) {
+        return (CashAccount) this.repository.retrieveTradeAccount(id);
+    }
+
+    public void createTradeAccount(CashAccount cashAccount) {
+        this.repository.createTradeAccount(cashAccount);
+    }
+
+    public void updateTradeAccount(CashAccount cashAccount) {
+        this.repository.updateTradeAccount(cashAccount);
+    }
+
+    public void deleteTradeAccount(String id) {
+        this.repository.deleteTradeAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        CashAccount cashAccount = retrieveTradeAccount(id);
+        cashAccount.getCashBalance().add(amount);
+        updateTradeAccount(cashAccount);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        CashAccount cashAccount = retrieveTradeAccount(id);
+        cashAccount.getCashBalance().subtract(amount);
+        updateTradeAccount(cashAccount);
+    }
 
 }
