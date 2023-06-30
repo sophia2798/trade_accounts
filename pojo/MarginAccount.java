bffae9f2eb3b8c94acc1c3164e68b629f0ff8845
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 00:40:18 -0700

    instantiated fields and methods for MarginAccount child class, override clone() method

--- pojo/MarginAccount.java
+++ pojo/MarginAccount.java
@@ -1,6 +1,27 @@
 package pojo;
 
-public class MarginAccount  {
+import java.math.BigDecimal;
 
+public class MarginAccount extends TradeAccount {
+
+    private BigDecimal margin;
+
+    public MarginAccount(String id, BigDecimal margin) {
+        super(id);
+        this.margin = margin;
+    }
+
+    public BigDecimal getMargin() {
+        return this.margin;
+    }
+
+    public void setMargin(BigDecimal margin) {
+        this.margin = margin;
+    }
+
+    @Override
+    public TradeAccount clone() {
+        return new MarginAccount(super.getId(), this.margin);
+    }
 
 }
