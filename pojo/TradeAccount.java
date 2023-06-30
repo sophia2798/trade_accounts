85b9436bf9e2478dadc4cb0919be2707d3e96d2f
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-06-30 00:34:02 -0700

    initialized field, getter, setter, and abstract clone method for TradeAccount superclass

--- pojo/TradeAccount.java
+++ pojo/TradeAccount.java
@@ -2,5 +2,20 @@ package pojo;
 
 public abstract class TradeAccount {
 
+    private String id;
+
+    public TradeAccount(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public abstract TradeAccount clone();
 
 }
